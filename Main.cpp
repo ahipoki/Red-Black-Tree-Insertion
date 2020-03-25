3844d86f183e5daa1617672df95a2d91b3d3e2a2
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-03-25 11:35:53 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include "Node.h"
+#include "RBT.h"
 
 using namespace std;
 
 void strupper(char* str);
+int strToInt(char* str);
+void Insert(RBT*);
+void Read(RBT*);
+void PrintTree(RBT*);
 
 int main()
 {
+  RBT* rbt = new RBT();
   char input[80];
   bool running = true;
   while (running == true){
@@ -14,13 +24,13 @@ int main()
     cin.getline(input, sizeof(input));
     strupper(input);
     if (strcmp(input, "ADD") == 0){
-      cout<<"Add"<<endl;
+      Insert(rbt);
     }
     else if (strcmp(input, "READ") == 0){
-      cout<<"READ"<<endl;
+      Read(rbt);
     }
     else if (strcmp(input, "PRINT") == 0){
-      cout<<"Print"<<endl;
+      PrintTree(rbt);
     }
     else if (strcmp(input, "QUIT") == 0){
       running = false;
@@ -29,6 +39,7 @@ int main()
       cout<<"That's an invalid option"<<endl;
     }
   }
+  delete rbt;
 }
 
 void strupper(char* str){
@@ -36,4 +47,45 @@ void strupper(char* str){
   for (int i = 0; i < len; i++){
     str[i] = toupper(str[i]);
   }
+}
+
+int strToInt(char* str){
+    int num = 0;
+    for (int i = 0; i < strlen(str); i++){
+        if (!isdigit(str[i])){
+            continue;
+        }
+        num *= 10;
+        num += str[i] - '0';
+    }
+    return num;
+}
+
+void Insert(RBT* rbt){
+    int key;
+    cout<<"What number do you want to add to the tree?"<<endl;
+    cin>>key;
+    cin.clear();
+    cin.ignore(999, '\n');
+    rbt->Insert(key);
+}
+
+void Read(RBT* rbt){
+    char fileName[80];
+    char line[100];
+    cout<<"Enter a file name"<<endl;
+    cin.getline(fileName, sizeof(fileName));
+    ifstream fileStream;
+    fileStream.open(fileName);
+    if (fileStream){
+        fileStream.getline(line, sizeof(line));
+    }
+    else{
+        cout<<"Couldn't find that file"<<endl;
+    }
+    fileStream.close();
+}
+
+void PrintTree(RBT* rbt){
+    rbt->PrintTree();
 }
