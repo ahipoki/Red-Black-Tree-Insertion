dd0c90063ba293e6ea9de57c53a9e479876eb7b3
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-03-25 11:36:45 -0700

    Create RBT.cpp

---
+++ RBT.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "RBT.h"
+#include "Node.h"
+
+using namespace std;
+
+RBT::RBT(){
+    head = NULL;
+}
+
+void RBT::Insert(int n){
+    Insert(head, n);
+}
+
+void RBT::Insert(Node* &h, int key){
+    if (h == NULL){
+        head = new Node(key);
+        return;
+    }
+    else if (key < h->getKey()){
+        if (h->getLeft() == NULL){
+            h->setLeft(new Node(key));
+            h->getLeft()->setParent(h);
+            return;
+        }
+        Node* left = h->getLeft();
+        Insert(left, key);
+        return;
+    }
+    else{
+        if (h->getRight() == NULL){
+            h->setRight(new Node(key));
+            h->getRight()->setParent(h);
+            return;
+        }
+        Node* right = h->getRight();
+        Insert(right, key);
+        return;
+    }
+}
+
+void RBT::PrintTree(){
+    PrintTree(head, 0);
+}
+
+void RBT::PrintTree(Node* h, int d){
+    if (!h){
+        return;
+    }
+    if (h->getLeft()){
+        PrintTree(h->getLeft(), d+1);
+    }
+    for (int i = 0; i < d; i++){
+        cout<<"   ";
+    }
+    cout<<h->getKey()<<endl;
+    if (h->getRight()){
+        PrintTree(h->getRight(), d+1);
+    }
+}
+
+RBT::~RBT(){
+
+}
