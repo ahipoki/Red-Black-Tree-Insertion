bb362adb57a577ecb4ef1b3a21a066e892b36621
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-04-03 12:30:00 -0700

    Update Node.h

--- Node.h
+++ Node.h
@@ -9,19 +9,29 @@
 using namespace std;
 
 enum Color {RED, BLACK};
+//Colors
 
 class Node{
     public:
       Node(int);
+      //Node
       ~Node();
       void setKey(int);
+      //Set key
       int getKey();
+      //Get key
       void setLeft(Node*);
+      //Set left
       Node* getLeft();
+      //Get left
       void setRight(Node*);
+      //Set right
       Node* getRight();
+      //Get right
       void setParent(Node*);
+      //Set parent
       Node* getParent();
+      //Get parent
       //Node* getUncle(Node*);
       //Node* getSibling(Node* );
       int color;
