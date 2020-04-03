6d607075876c26d04ee55835b897a2bc6e1fde93
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-04-03 12:31:13 -0700

    Update RBT.h

--- RBT.h
+++ RBT.h
@@ -5,19 +5,32 @@
 class RBT{
     public:
       RBT();
+      //Red-black tree
       void Insert(int);
+      //Insert
       void Read();
+      //Read
       void PrintTree();
+      //Print tree
       ~RBT();
+      //Destructor
     private:
       Node* root;
+      //Root node
     protected:
       void rotateLeft(Node*&);
+      //Rotate left
       void rotateRight(Node*&);
+      //Rotate right
       void fixInsert(Node*&);
+      //Fix w/insert
       int getColor(Node*&);
+      //Get color of node
       void setColor(Node*&, int);
+      //Set color of node
       Node* Insert(Node*&, Node*&);
+      //Insert
       void PrintTree(Node*, int);
+      //Print tree
 };
 #endif
