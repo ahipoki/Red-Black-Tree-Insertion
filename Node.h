31dc7b303bd13b01da2f1a946ef057e399e8b4fa
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-04-23 14:27:41 -0700

    Update Node.h

--- Node.h
+++ Node.h
@@ -3,6 +3,28 @@
 #include <iostream>
 
 class Node{
-
+  public:
+    Node(int);
+    int getKey();
+    void setKey(int);
+    Node* getLeft();
+    void setLeft(Node*);
+    Node* getRight();
+    void setRight(Node*);
+    Node* getParent();
+    void setParent(Node*);
+    Node* getUncle();
+    Node* getSibling();
+    void toggleColor();
+    void rotateLeft();
+    void rotateRight();
+    bool isRed();
+    ~Node();
+  private:
+    Node* Left = NULL;
+    Node* Right = NULL;
+    Node* Parent = NULL;
+    int key;
+    bool isred;
 };
 #endif
