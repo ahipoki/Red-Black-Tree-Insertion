f17d970132f41b7f40658a9f64078341823b9973
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-04-23 17:37:46 -0700

    Update RBT.h

--- RBT.h
+++ RBT.h
@@ -15,8 +15,12 @@ class RBT{
       Node* root;
       //Root node
       void Insert(Node*&, Node*);
+      //Private insert function
       void PrintTree(Node*, int);
+      //Private printtree function
       void fixTree(Node*);
+      //Private fixtree function
       void deleteTree(Node*);
+      //Delete the tree
 };
 #endif
