551865e835e631018b8385854f7d2d388d8a9e13
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-04-23 17:36:21 -0700

    Update Node.h

--- Node.h
+++ Node.h
@@ -5,26 +5,47 @@
 class Node{
   public:
     Node(int);
+    //Constructor
     int getKey();
+    //Get key value
     void setKey(int);
+    //Set key value
     Node* getLeft();
+    //Get left node
     void setLeft(Node*);
+    //Set left node
     Node* getRight();
+    //Get right node
     void setRight(Node*);
+    //Set right node
     Node* getParent();
+    //Get parent node
     void setParent(Node*);
+    //Set parent node
     Node* getUncle();
+    //Get uncle node
     Node* getSibling();
+    //Get sibling node
     void toggleColor();
+    //Change color of node
     void rotateLeft();
+    //Rotate tree left
     void rotateRight();
+    //Rotate tree right
     bool isRed();
+    //Check color of node
     ~Node();
+    //Destructor
   private:
-    Node* Left = NULL;
-    Node* Right = NULL;
-    Node* Parent = NULL;
+    Node* Left;
+    //Left node
+    Node* Right;
+    //Right node
+    Node* Parent;
+    //Parent node
     int key;
+    //Key value
     bool isred;
+    //Color of node
 };
 #endif
